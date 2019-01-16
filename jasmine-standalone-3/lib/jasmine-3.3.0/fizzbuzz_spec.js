aafe383046cbab2e31dc24b2fb65d36fd76ff30c
Alex <alikhanialex@gmail.com>
2019-01-16 15:29:22 +0000

    test passing, multiples of five return buzz

--- jasmine-standalone-3/lib/jasmine-3.3.0/fizzbuzz_spec.js
+++ jasmine-standalone-3/lib/jasmine-3.3.0/fizzbuzz_spec.js
@@ -1,10 +1,10 @@
-describe('fizzBuzz', function () {
+describe("fizzBuzz", function() {
 
   it("should return fizz for multiples of three", function() {
     expect(fizzBuzz(3)).toEqual("fizz");
   });
 
   it("should return buzz for multiples of five", function() {
-     expect(fizzBuzz(5)).toEqual("buzz")
-   });
-});
+    expect(fizzBuzz(5)).toEqual("buzz")
+  });
+ });
--- jasmine-standalone-3/src/fizzbuzz.js
+++ jasmine-standalone-3/src/fizzbuzz.js
@@ -2,4 +2,7 @@ function fizzBuzz(num){
    if(num%3 === 0){
     return("fizz");
     }
+    else if(num%5 === 0){
+     return("buzz");
+     }
   }
